31368892169e134342d581db225d0ee0f35f7f78
Moinnawaz03 <141616375+Moinnawaz03@users.noreply.github.com>
2023-09-05 16:28:18 +0530

    first commit

---
+++ scrip.js
@@ -0,0 +1,35 @@
+function One() {
+  document.body.style.backgroundColor = "blue";
+  document.body.style.color = "white";
+  document.getElementById("nav").style.backgroundColor = "grey";
+}
+function Two() {
+  document.body.style.backgroundColor = "grey";
+  document.body.style.color = "black";
+  document.getElementById("nav").style.backgroundColor = "blue";
+}
+function Theree() {
+  document.body.style.backgroundColor = "green";
+  document.body.style.color = "white";
+  document.getElementById("nav").style.backgroundColor = "red";
+}
+function fore() {
+  document.body.style.backgroundColor = "red";
+  document.body.style.color = "white";
+  document.getElementById("nav").style.backgroundColor = "green";
+}
+function five() {
+  document.body.style.background = "yellow";
+  document.body.style.color = "back";
+  document.getElementById("nav").style.backgroundColor = "aqua";
+}
+function six() {
+  document.body.style.background = "aqua";
+  document.body.style.color = "black";
+  document.getElementById("nav").style.backgroundColor = "yellow";
+}
+function seven() {
+  document.body.style.background = "black";
+  document.body.style.color = "white";
+  document.getElementById("nav").style.backgroundColor = "aqua";
+}
